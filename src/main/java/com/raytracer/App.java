2bf17e99fc9ee360d0e0f9b46e66ad72521fd61c
Rubab Hossain <rubabzh97@gmail.com>
2020-08-09 18:45:33 -0400

    technically drawing using a 3d algorithm, but result looks 2d

--- src/main/java/com/raytracer/App.java
+++ src/main/java/com/raytracer/App.java
@@ -6,26 +6,31 @@ import com.raytracer.lib.linalg.Matrices;
 import com.raytracer.lib.linalg.Matrix;
 import com.raytracer.lib.primitives.Color;
 import com.raytracer.lib.primitives.Point;
+import com.raytracer.lib.primitives.Vector;
+import com.raytracer.lib.world.Ray;
+import com.raytracer.lib.world.Sphere;
 
 
 public class App {
-    static int height = 100, width = 100;
+    static int height = 1000, width = 1000;
     static Canvas canvas = new Canvas(height, width);
     static Color WHITE = new Color(1,1,1);
 
     public static void main( String[] args ) {
 
-        Matrix rotationMtx = Matrices.Identity(4).rotateZ(Math.PI / 6);
-        Matrix translationMtx = Matrices.Identity(4).translate(50, 50, 0);
+        Sphere s = (new Sphere()).transform(Matrices.scale(400,100,100))
+                                 .transform(Matrices.translation(500, 500, 0));
 
-        Point p = new Point(0, 40, 0);
+        Point rayOrigin = Matrices.translation(500,500,0).mult(new Point(0,0,200));
 
-        for(int i = 0; i < 12; i++) {
-            Point drawP = translationMtx.mult(p);
-            canvas.setPixel(height - (int) Math.round(drawP.getY()),
-                            (int) Math.round(drawP.getX()),
-                            WHITE);
-            p = rotationMtx.mult(p);
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                Vector dir = rayOrigin.subtract(new Point(j, i, 0)).negate();
+                Ray r = new Ray(rayOrigin, dir);
+                if(r.getIntersections(s).getHit().isPresent()) {
+                    canvas.setPixel(j, i, WHITE);
+                }
+            }
         }
 
         System.out.println(PpmWriter.toFmtString(canvas));

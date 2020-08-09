ad04c913ad82192d1953426705a6121582577993
Rubab Hossain <rubabzh97@gmail.com>
2020-08-08 21:36:30 -0400

    impl simple POC of matrix tranforms

--- src/main/java/com/raytracer/App.java
+++ src/main/java/com/raytracer/App.java
@@ -2,6 +2,8 @@ package com.raytracer;
 
 import com.raytracer.lib.canvas.Canvas;
 import com.raytracer.lib.canvas.PpmWriter;
+import com.raytracer.lib.linalg.Matrices;
+import com.raytracer.lib.linalg.Matrix;
 import com.raytracer.lib.primitives.Color;
 import com.raytracer.lib.primitives.Point;
 import com.raytracer.lib.primitives.Vector;
@@ -10,51 +12,29 @@ import lombok.Value;
 
 
 public class App {
+    static int height = 100, width = 100;
+    static Canvas canvas = new Canvas(height, width);
+    static Color WHITE = new Color(1,1,1);
 
     public static void main( String[] args ) {
-        
-        drawArc();
-    }
 
+        Matrix rotationMtx = Matrices.Identity(4)
+                                     .rotateZ(Math.PI / 6);
+        Matrix translationMtx = Matrices.Identity(4)
+                                     .translate(50, 50, 0);
+        Point p = new Point(0, 40, 0);
 
-    public static void drawArc() {
-        int height = 550, width = 900;
+        for(int i = 0; i < 12; i++) {
+            Point drawP = translationMtx.mult(p);
+            canvas.setPixel(height - (int) Math.round(drawP.getY()),
+                            (int) Math.round(drawP.getX()),
+                            WHITE);
 
-        Canvas canvas = new Canvas(height, width);
-        Color white = new Color(1, 1, 1);
-
-        Point startingPos = new Point(0, 1, 0);
-        Vector startingVel = new Vector(1, 1.8, 0).normalize().mult(11.25);
-        Projectile prj = new Projectile(startingPos, startingVel);
-
-        Vector g = new Vector(0, -0.1, 0);
-        Vector w = new Vector(-0.01, 0, 0);
-        Environment e = new Environment(g, w);
-
-        while(prj.getPosition().getY() > 0) {
-            int x = (int) Math.round(prj.getPosition().getX());
-            int y =  (int) Math.round(prj.getPosition().getY());
-            canvas.setPixel(height - y, x, white);
-            prj = tick(prj, e);
+            p = rotationMtx.mult(p);
         }
 
         System.out.println(PpmWriter.toFmtString(canvas));
     }
 
-
-    public static Projectile tick(Projectile p, Environment e) {
-        Point pos = p.getPosition();
-        Vector vel = p.getVelocity();
-        Vector g = e.getGravity();
-        Vector w = e.getWind();
-
-        Point newPos = pos.add(vel);
-        Vector newVel = vel.add(g).add(w);
-        return new Projectile(newPos, newVel);
-    }
-
-    @Value @RequiredArgsConstructor public static class Projectile { Point position; Vector velocity; }
-
-    @Value @RequiredArgsConstructor public static class Environment { Vector gravity; Vector wind; }
 
 }
